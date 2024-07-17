821e6c12060860ced9eb64de0ba604b403340aa2
Akshayp76 <118371624+Akshayp76@users.noreply.github.com>
2024-07-17 13:36:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import navbar from '.components/navbar.js';
+function App() {
+  return (
+    <>
+      <navbar/>
+    </>
+  );
+}
+
+export default App;
